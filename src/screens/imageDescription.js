017864ca1c9eca292561d0a4f730a7910afcd70e
Mateusz Goździk <Mateusz.Gozdzik.pl@gmail.com>
2023-11-13 22:07:36 +0100

    add some info dto img desc

--- src/screens/imageDescription.js
+++ src/screens/imageDescription.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const ImageDescription = (props) => {
   const [data, setData] = useState(null);
@@ -12,7 +12,7 @@ const ImageDescription = (props) => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        `https://api.artic.edu/api/v1/artworks/${itemId}?fields=title,image_id`
+        `https://api.artic.edu/api/v1/artworks/${itemId}?fields=title,image_id,date_display,place_of_origin,artist_titles`
       );
       const responseData = await response.json();
       const newData = responseData.data;
@@ -25,28 +25,43 @@ const ImageDescription = (props) => {
   if (data) {
     return (
       <View style={styles.container}>
-        <View style={styles.wrapper}>
-          <ImageBackground
-            source={{
-              uri: `https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`
-            }}
-            style={styles.image}
-          >
-            <Text style={styles.text}>{data.title}</Text>
-          </ImageBackground>
+        <Image
+          source={{
+            uri: `https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>{data.title}</Text>
+        <View style={styles.infoWrapper}>
+          <Text style={{ color: 'white' }}>{data.place_of_origin}</Text>
+          <Text style={{ color: 'white', textAlign: 'left' }}>
+            {data.date_display}
+          </Text>
+          <Text style={{ color: 'white', textAlign: 'left' }}>
+            {data.artist_titles}
+          </Text>
         </View>
       </View>
     );
   }
 };
 const styles = StyleSheet.create({
-  container: { backgroundColor: 'black', flex: 1 },
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    alignItems: 'center'
+  },
   image: {
     width: 300,
     height: 500,
-    justifyContent: 'flex-end'
+    flex: 4
   },
-  text: {},
-  wrapper: { flex: 1, alignItems: 'center' }
+  title: { flex: 1, fontSize: 30, fontWeight: 'bold', color: 'white' },
+  infoWrapper: {
+    flex: 1,
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  }
 });
 export default ImageDescription;
