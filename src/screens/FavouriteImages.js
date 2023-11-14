38a1e8a0c08642454eb56ecfccfe91794150989b
Mateusz Goździk <Mateusz.Gozdzik.pl@gmail.com>
2023-11-14 15:24:01 +0100

    fix favourites

--- src/screens/FavouriteImages.js
+++ src/screens/FavouriteImages.js
@@ -7,7 +7,6 @@ const FavImages = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
-  const [usedIds, setUsedIds] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -17,30 +16,21 @@ const FavImages = () => {
     if (!loading)
       try {
         setLoading(true);
-        const favImageIds = await loadLikedItems();
-
-        if (data.length === favImageIds.length) {
-          setLoading(false);
+        const favImages = await loadLikedItems();
+        imageIdsToFetch = favImages.slice((page - 1) * 15, page * 15);
+        console.log(imageIdsToFetch);
+        if (imageIdsToFetch.length === 0) {
           return;
         }
-
-        const newIds = usedIds
-          ? favImageIds.filter((id) => !usedIds.includes(id))
-          : favImageIds;
-        const newIdsString = newIds.join(',');
-
         const response = await fetch(
-          `https://api.artic.edu/api/v1/artworks?limit=15&page=${page}&fields=title,api_link,image_id,id&ids=${newIdsString}`
+          `https://api.artic.edu/api/v1/artworks?fields=title,api_link,image_id,id&ids=${imageIdsToFetch}`
         );
         const responseData = await response.json();
         const newData = responseData.data;
-        const usedIds = newData.map((item) => item.id);
 
         if (page === 1) {
-          setUsedIds(usedIds);
           setData(newData);
         } else {
-          setUsedIds((prevUsedIds) => [...prevUsedIds, ...usedIds]);
           setData((prevData) => [...prevData, ...newData]);
         }
 
