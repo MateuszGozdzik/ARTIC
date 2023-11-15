2580864d93eb6094c5a45a495dca0439adaf35b6
Mateusz Goździk <Mateusz.Gozdzik.pl@gmail.com>
2023-11-15 16:48:13 +0100

    zoomable only if is_zoomable

--- src/screens/imageDescription.js
+++ src/screens/imageDescription.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import ImageViewer from 'react-native-image-zoom-viewer';
 
 const ImageDescription = ({ route }) => {
@@ -13,7 +13,7 @@ const ImageDescription = ({ route }) => {
   const fetchData = async () => {
     try {
       const response = await fetch(
-        `https://api.artic.edu/api/v1/artworks/${itemId}?fields=title,image_id,date_display,place_of_origin,artist_titles`
+        `https://api.artic.edu/api/v1/artworks/${itemId}?fields=title,image_id,date_display,place_of_origin,artist_titles,is_zoomable`
       );
       const responseData = await response.json();
       const newData = responseData.data;
@@ -26,23 +26,45 @@ const ImageDescription = ({ route }) => {
   if (data) {
     const imageUri = `https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`;
 
-    return (
-      <View style={styles.container}>
+    const Img = () => {
+      return data.is_zoomable ? (
         <ImageViewer
           imageUrls={[{ url: imageUri }]}
           renderHeader={() => <Text style={styles.title}>{data.title}</Text>}
-          style={styles.imageViewer}
+          style={styles.image}
           renderIndicator={() => null}
         />
-        <Text style={{ color: 'white', fontSize: 18 }}>
-          {data.place_of_origin}
-        </Text>
-        <Text style={{ color: 'white', textAlign: 'left', fontSize: 16 }}>
-          {data.date_display}
-        </Text>
-        <Text style={{ color: 'white', textAlign: 'left', fontSize: 16 }}>
-          {data.artist_titles}
-        </Text>
+      ) : (
+        <Image
+          style={styles.image}
+          source={{
+            uri: imageUri
+          }}
+        />
+      );
+    };
+
+    return (
+      <View style={styles.container}>
+        <Img />
+        <View style={styles.descWrapper}>
+          <Text
+            style={{
+              color: 'white',
+              textAlign: 'left',
+              fontSize: 16,
+              fontWeight: 'bold'
+            }}
+          >
+            {data.artist_titles}
+          </Text>
+          <Text style={{ color: 'white', fontSize: 18 }}>
+            {data.place_of_origin}
+          </Text>
+          <Text style={{ color: 'white', textAlign: 'left', fontSize: 16 }}>
+            {data.date_display}
+          </Text>
+        </View>
       </View>
     );
   }
@@ -62,9 +84,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     paddingTop: 20
   },
-  imageViewer: {
-    height: 'auto',
+  image: {
     flex: 2
+  },
+  descWrapper: {
+    flex: 1
   }
 });
 
