9270bfc99fc831ca9c1e2c120000591067626e4a
Mateusz Goździk <Mateusz.Gozdzik.pl@gmail.com>
2023-11-15 16:28:10 +0100

    refactor

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
-import { loadLikedItems } from '../utilities/Storage';
+import React from 'react';
+import { FlatList, ActivityIndicator } from 'react-native';
 import ListItem from './ListItem';
 
 const List = ({ data, loading, fetchData }) => {
-  const [likedItems, setLikedItems] = useState([]);
-
-  useEffect(() => {
-    const loadLikedItemsAsync = async () => {
-      const items = await loadLikedItems();
-      setLikedItems(items);
-    };
-
-    loadLikedItemsAsync();
-  }, []);
-
   return (
     <FlatList
       data={data}
