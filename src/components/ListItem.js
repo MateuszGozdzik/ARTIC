0d2656f019a996df268d0e59971d1a6edc10f4f7
Mateusz Goździk <Mateusz.Gozdzik.pl@gmail.com>
2023-11-19 10:50:19 +0100

    change ListItem styling

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import LikedButton from './LikedButton';
 
@@ -10,42 +16,40 @@ const ListItem = ({ itemData }) => {
     navigation.navigate('ImageDescription', { itemId: itemData.id });
   };
 
+  const imageUri = itemData.image_id
+    ? {
+        uri: `https://www.artic.edu/iiif/2/${itemData.image_id}/full/843,/0/default.jpg`
+      }
+    : require('../../assets/no-image.jpeg');
+
   return (
     <TouchableOpacity onPress={handleItemPress}>
-      <View style={styles.item}>
-        {itemData.image_id ? (
-          <Image
-            style={styles.image}
-            source={{
-              uri: `https://www.artic.edu/iiif/2/${itemData.image_id}/full/843,/0/default.jpg`
-            }}
-          />
-        ) : (
-          <Image
-            style={styles.image}
-            source={require('../../assets/amongus.png')}
-          />
-        )}
-        <Text style={styles.text}>{itemData.title}</Text>
-        <LikedButton itemId={itemData.id} />
-      </View>
+      <ImageBackground source={imageUri} style={styles.imageBackground}>
+        <View style={styles.overlay}>
+          <Text style={styles.text}>{itemData.title}</Text>
+          <LikedButton itemId={itemData.id} style={styles.icon} />
+        </View>
+      </ImageBackground>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: 'grey',
-    padding: 5,
-    paddingHorizontal: 20,
-    margin: 3,
-    borderRadius: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+  imageBackground: {
+    resizeMode: 'contain',
+    justifyContent: 'center',
+    height: 250
   },
-  image: { width: 50, height: 50, borderRadius: 10 },
-  text: { maxWidth: 200, textAlign: 'center' }
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'flex-end',
+    padding: 16
+  },
+  text: {
+    color: 'white',
+    fontSize: 24
+  }
 });
 
 export default ListItem;
